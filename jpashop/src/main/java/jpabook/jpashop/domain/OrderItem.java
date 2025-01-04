e808c352a2e0966574bd5af0d4fe5957004142d3
bangddong <gusehdqkd@naver.com>
2025-01-04 17:57:55 +0900

    feat : initialized  - 객체지향이 아닌 DB 지향적 설계 코드

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,65 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ORDER_ID")
+    private Long order;
+
+    @Column(name = "ITEM_ID")
+    private Long item;
+
+    private int orderPrice;
+
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrder() {
+        return order;
+    }
+
+    public void setOrder(Long order) {
+        this.order = order;
+    }
+
+    public Long getItem() {
+        return item;
+    }
+
+    public void setItem(Long item) {
+        this.item = item;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
