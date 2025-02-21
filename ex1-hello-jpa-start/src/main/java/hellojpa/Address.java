8e5d8e023a5193518138138cd43c19c3aabe898d
dhbang <gusehdqkd@naver.com>
2025-02-21 21:35:04 +0900

    값 타입 비교

--- ex1-hello-jpa-start/src/main/java/hellojpa/Address.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/Address.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import java.util.Objects;
+
 import jakarta.persistence.Embeddable;
 
 @Embeddable
@@ -39,5 +41,19 @@ public class Address {
 
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Address address = (Address)o;
+		return Objects.equals(city, address.city) && Objects.equals(street, address.street)
+			&& Objects.equals(zipcode, address.zipcode);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(city, street, zipcode);
 	}
 }
